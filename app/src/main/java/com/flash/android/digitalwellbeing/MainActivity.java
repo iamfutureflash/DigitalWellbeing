16ddf81ba7ffb3af39cdb342dda44929332b3279
iamfutureflash <xenode2000@gmail.com>
2022-08-10 03:36:13 +0530

    started

---
+++ app/src/main/java/com/flash/android/digitalwellbeing/MainActivity.java
@@ -0,0 +1,14 @@
+package com.flash.android.digitalwellbeing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
